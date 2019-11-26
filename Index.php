cc8b076dc6e781f3514632a2b0ee52b6106537c3
brodriguezm998 <53025603+brodriguezm998@users.noreply.github.com>
2019-11-26 15:04:17 -0500

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,37 +1,16 @@
-<?php if(isset($_SESSION['access_token'])):?>
-<?php include("Acceder-Camara-Movil.html");
-?>
-
-<?php else:?>
-	
+<!DOCTYPE html>
 <html lang="es">
-	<head>
-		<title>WEB APP NASA</title>
-		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-		
-		<link rel="stylesheet" href="css/main.css">
-		<style type="text/css">
-				.container{
-					height: 450px;
-				}
-				#map{
-					align: center;
-					width: 500px;
-					height: 550px;
-					border: 1px solid blue;
-				}
-			</style>
-		
-	</head>
+	<?php include("./header.php");?>
 	<body>
-		<div id="map"class="container">
-			<h1>WELCOME WEB APP</h1>
-			<?php include("./Google/index.php");?>
+		<div id="map"class="container-fluid">
+			<div class="row">
+				<div class="col-12 border2">
+					
+				</div>
+			</div>
 		</div>
+		<script src="js/jquery-3.3.1.slim.min.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</body>	
-	
-<?php endif;?>
+<html>
---
+++ header2.php
@@ -0,0 +1,45 @@
+<head>
+		<title>WEB APP NASA</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie-edge">
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<style type="text/css">
+			.border{
+				background-color: Lightgreen;
+				height: 110px;
+				text-align: center;
+			}
+			.border1{
+				background-color: Lightgreen;
+				height: 110px;
+				text-align: center;
+			}
+			.border2{
+				height: 108px;
+				text-align: center;
+			}
+			.border3{
+				height: 50px;
+				text-align: center;
+			}
+			.border4{
+				background-color: Lightgreen;
+				height: 150px;
+				text-align: left;
+			}
+		</style>
+		<div id="icon" class="container-fluid">
+			<div class="row">
+				<div class="col-1 border">
+					<br><img src="../img/wofiex2.png" class="rounded mx-auto d-block" width="100%" height="auto">
+				</div>
+				<div class="col-10 border1">
+					<br><h1>WELCOME TO <strong>WOFIEX</strong></h1>
+				</div>
+				<div class="col-1 border">
+					<br><a class="btn btn-danger" href="./logout.php">Sign out</a>
+				</div>
+			</div>
+		</div>
+	</head>
